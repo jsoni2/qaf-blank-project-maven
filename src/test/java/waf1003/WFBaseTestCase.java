704b3e15a5d4c49bc5ddeb4608959dde12a70b8d
Janak Soni <janak.soni@infostretch.com>
2018-11-19 09:52:18 -0800

    WAF_1003

---
+++ src/test/java/waf1003/WFBaseTestCase.java
@@ -0,0 +1,5 @@
+package waf1003;
+
+public class WFBaseTestCase {
+
+}
---
+++ src/test/java/waf1003/waf1003.java
@@ -0,0 +1,14 @@
+package waf1003;
+
+public class waf1003 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BaseTestCase btc1 = new BaseTestCase();
+		
+		
+		System.out.println(btc1.getClass().getSuperclass().getName());
+
+	}
+
+}
