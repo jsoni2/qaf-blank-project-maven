f423aab28ea0ae73cffa8193b769b1286ce8f7b8
Janak Soni <janak.soni@infostretch.com>
2018-11-19 11:01:44 -0800

    Java_1004

---
+++ src/test/java/java/java1004/Java1004.java
@@ -0,0 +1,12 @@
+package java.java1004;
+
+import javax.swing.JOptionPane;
+
+public class Java1004 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane pane = new JOptionPane();
+	}
+
+}
