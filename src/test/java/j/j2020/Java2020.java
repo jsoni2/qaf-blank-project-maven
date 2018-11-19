c2c1dda46f3d31e333f3e06e86f48ee7a78acc62
Janak Soni <janak.soni@infostretch.com>
2018-11-19 12:34:23 -0800

    Java_2020

---
+++ src/test/java/j/j2020/Java2020.java
@@ -0,0 +1,28 @@
+package j.j2020;
+
+import static org.hamcrest.CoreMatchers.either;
+
+import java.io.FileReader;
+import java.io.Reader;
+
+public class Java2020 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		try {
+			Reader fileReader = new FileReader("/Users/janak.soni/work/wf/qaf-blank-project-maven/src/test/java/j/j2020/input-text.txt");
+			int data =  fileReader.read();
+			while(data != -1) {
+				System.out.println(data);
+				data = fileReader.read();
+			}
+			fileReader.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
