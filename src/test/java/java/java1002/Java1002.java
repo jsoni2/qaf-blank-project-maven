f5ade782ddd4e8cecf547fb170d52c9c38cd8f65
Janak Soni <janak.soni@infostretch.com>
2018-11-19 10:52:54 -0800

    Java_1002

---
+++ src/test/java/java/java1002/Java1002.java
@@ -0,0 +1,11 @@
+package java.java1002;
+
+
+public class Java1002 {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		Thread.sleep(2000);
+	}
+
+}
