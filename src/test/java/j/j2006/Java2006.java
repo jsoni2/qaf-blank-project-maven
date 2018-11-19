fef9bb3f6ea33fa398951ccc5dfc2b08f44f8e97
Janak Soni <janak.soni@infostretch.com>
2018-11-19 12:01:41 -0800

    Java_2006

---
+++ src/test/java/j/j2006/Java2006.java
@@ -0,0 +1,16 @@
+package j.j2006;
+
+public class Java2006 {
+	
+	static int bar;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		bar = 10;
+		System.out.println(bar);
+		bar = bar + 1;
+		System.out.println(bar);
+		
+	}
+
+}
