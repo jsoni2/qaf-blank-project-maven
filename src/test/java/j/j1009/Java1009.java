e08ac99f256a3cc514047be02521f38a1333292c
Janak Soni <janak.soni@infostretch.com>
2018-11-19 11:11:38 -0800

    Java_1009

---
+++ src/test/java/j/j1009/Java1009.java
@@ -0,0 +1,14 @@
+package j.j1009;
+
+import com.qmetry.qaf.automation.ui.WebDriverTestCase;
+
+public class Java1009 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverTestCase webDriverTestCase = new WebDriverTestCase();
+		webDriverTestCase.getClass();
+
+	}
+
+}
