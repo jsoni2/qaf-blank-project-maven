71e346f52062122d2be78a826d3dd73ba01c5283
Janak Soni <janak.soni@infostretch.com>
2018-11-19 09:21:31 -0800

    WAF_1002 - Update

--- src/test/java/waf1002/waf1002.java
+++ src/test/java/waf1002/waf1002.java
@@ -1,14 +1,19 @@
 package waf1002;
 
+
 public class waf1002 {
 	
-	// To Pass:
-	WFWebElement ele1 = new WFWebElement();
 	
-	// To Pass:
-	WFDropDownListWebElement ele2 = new WFDropDownListWebElement();
-	
-	// To Fail:
-	String ele3 = "";
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// To Pass:
+		WFWebElement ele1 = new WFWebElement();
+		
+		// To Pass:
+		WFDropDownListWebElement ele2 = new WFDropDownListWebElement();
+		
+		// To Fail:
+		String ele3 = "";
+	}
 
 }
