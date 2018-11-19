e4307e046e25c6945a652e7d52231dc2b1b6a705
Janak Soni <janak.soni@infostretch.com>
2018-11-19 11:24:24 -0800

    Java_2002

---
+++ src/test/java/j/j2002/Java2002.java
@@ -0,0 +1,16 @@
+package j.j2002;
+
+public class Java2002 {
+	
+	public Object GetEmptyString() {
+		return null;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Java2002 java2002 = new Java2002();
+		java2002.GetEmptyString();
+		System.out.println(java2002.GetEmptyString());
+	}
+
+}
