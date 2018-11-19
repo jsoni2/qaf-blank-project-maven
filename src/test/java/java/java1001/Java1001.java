7e97961f943b5916c349509c8fa11a20a924a638
Janak Soni <janak.soni@infostretch.com>
2018-11-19 10:45:30 -0800

    Java_1001

---
+++ src/test/java/java/java1001/Java1001.java
@@ -0,0 +1,16 @@
+package java.java1001;
+
+public class Java1001 {
+	
+	public void findElement() {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Java1001 java1001 = new Java1001();
+		java1001.findElement();
+	}
+
+}
